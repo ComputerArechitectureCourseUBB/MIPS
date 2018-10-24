d1bd70a7e90c8a2bd9e28b7cfade266cb8f5cb50
Krzysztof <kherman@ubiobio.cl>
2018-10-24 14:20:21 -0300

    Function call modified

--- asm/FunctionCall.asm
+++ asm/FunctionCall.asm
@@ -1,38 +1,44 @@
-# Reading memory 
+# Function call
+#  High level C code 
+#	  int sum(int a, int b){
+#		return a+b;
+#	  }
 
-.data 
-#offset:  .word 0x10010000
-a: .word 0x00000012
-b: .word 0x00000013
-c: .word 0x00000000
+# arguments
+# a = $a0
+# b = $a1
+# ret value $v0
 
-.text
-.global main 
-la  $t0, a
-la  $t1, b
-la  $t2, c
-lw $s1 0($t0) 
-lw $s2 0($t1)
-jal func  
-add $s3, $s2, $s1
-sw $s3, 0($t2)
+	.text
+	.global main 
+        li $a0, 22		# initialize a
+        li $a1, 4		# initialize b
+        li $s0, 255		# initialize $s0 to observe the stack
+        li $s1, 127		# initialize $s1 to observe the stack
+	jal funcli	    	# call the function 
+	move $s4, $v0  		# move result to the s4 register 
+	sw $s4, 0($0)		# save s4 into the memory
+	
+	move $a0, $s4	   	# move result to the a0 reg to display it    
+	li	$v0, 1		# configure syscall to print int from a0 register  
+	syscall			# call syscall
+	
 
-li	$v0, 10		# system call code for exit = 10
-syscall		
+	li	$v0, 10		# system call code for exit = 10
+	syscall			# call syscall
 
-func: 
-addi $sp, $sp, -8
-sw $s0, 4($sp)
-sw $s1, 0($sp)
-li $v0, 0x08
-ori $v1, $v0, 0x01
-xori $v1, $v1, 0x01
-xori $v1, $v1, 0x01
-xori $v1, $v1, 0x01
-lw $s0, 4($sp)
-lw $s1, 0($sp)
-addi $sp, $sp, 8
-jr $ra
+	func: 
+	addi $sp, $sp, -12	# allocate space for 3 variables
+	sw $ra, 8($sp)		# save return address in case of nested function call
+	sw $s0, 4($sp)		# save the s0 and s1 regs
+	sw $s1, 0($sp)		
+	add $s0, $a0, $a1	# perform the operations using s0 reg !!!!!!
+	move $v0, $s0 		# move the result to the  v0 reg 
+	lw $ra, 8($sp)		# recall the orginal register state 
+	lw $s0, 4($sp)	
+	lw $s1, 0($sp)
+	addi $sp, $sp, 12	# deallocate stac memory 	
+	jr $ra			# jump to the caller
  
 
 
